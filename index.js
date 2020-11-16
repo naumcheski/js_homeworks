26619536550315633c2f40eecc61f29cdb14cb31
naumcheski <72261890+naumcheski@users.noreply.github.com>
2020-11-16 19:11:08 +0100

    Homework 3

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-// Declare variables and use the assignment operators ( print the results)
+//1. Write a JavaScript conditional statement to sort three numbers. Print the result.
 
-let varOne = 18;
-let varTwo = 16;
-let varThree = 14;
-let varFour = 12;
-let varFive = 10;
+//2.Write a JavaScript conditional statement to find the largest of five numbers. Print the result.
 
-varOne %= 3;
-console.log(varFive);
-
-varFour += 4;
-console.log(varFour);
-
-varThree -= 7;
-console.log(varThree);
-
-varTwo *= 3;
-console.log(varTwo);
-
-varOne /= 3;
-console.log(varOne);
-
-
-// Try out the two kind of alerts that have been studied.
-
-window.alert('Prv ALERT');
-alert('Vtor ALERT');
-let printOut = confirm('ALERTTTT');
-console.log(printOut);
-
-// Try out the different types of loggings in the console
-
-console.error('Ова е ерор лог');
-console.table(['Лимон', 'Круши', 'Киви']);
-console.log(['Банани', 'Мандарини', 'Ананас']);
-let fruits = {name: 'авокадо', year: 2020};
-console.dir(fruits);
-console.info('Homework 2 JavaScript');
-console.time('script');
-setTimeout(() => {
-     console.timeEnd('script');
-}, 7000);
+a=5;
+b=-2;
+c=7;
+d= 0;
+f=-1;
+if (a>b && a>c && a>d && a>f)
+{
+    console.log(a);
+}
+else if (b>a && b>c && b>d && b>f)
+{
+    console.log(b);
+}
+else if (c>a && c>b && c>d && c>f)
+{
+    console.log(c);
+}
+else if (d>a && d>c && d>b && d>f)
+{
+    console.log(d);
+}
+else
+{
+    console.log(f);
+}
 
 
+// For a given hour, print out the corresponding greeting (Good morning, Good afternoon, Good evening)
+
+function myFunction() {
+    var greeting;
+    var time = new Date().getHours();
+    if (time < 10) {
+      greeting = "Добро утро!";
+    } else if (time < 20) {
+      greeting = "Добар ден!";
+    } else {
+      greeting = "Добра вечер!";
+    }
+    document.getElementById("demo").innerHTML = greeting;
+  }
+  
+  //For a given month, print out in which season it belongs.
+
+
+  function getSeason() {
+    month = document.forms.date.month.value;
+    season = '';
+    switch(month) {
+        case '12':
+        case '1':
+        case '2':
+            season = 'Зима';
+        break;
+        case '3':
+        case '4':
+        case '5':
+            season = 'Пролет';
+        break;
+        case '6':
+        case '7':
+        case '8':
+            season = 'Лето';
+        break;
+        case '9':
+        case '10': 
+        case '11':
+            season = 'Есен';
+        break;
+    }
+    alert(season);
+}
